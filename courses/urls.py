e2c6342a6573f1b62dab7b72e42c4f3649308f6e
pete <pkeen7@gmail.com>
2022-10-20 16:20:45 -0600

    Add URlConf and basic templates
    The URLs are for a many-to-one relationship between Course and Lesson.
    They would need revision for a many-to-many relationship.

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:course_slug>/', views.course_detail, name='course_detail'),
+    path('<str:course_slug>/<str:lesson_slug>', views.lesson_detail, name='lesson_detail')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, HttpResponse
+from .models import Course, Lesson
 
 # Create your views here.
 
@@ -8,6 +9,34 @@ def hello(request):
     return HttpResponse('hello')
 
 def index(request):
+    # Return list of courses
+    queryset = Course.objects.all()
 
-    return HttpResponse("Courses Index")
+    context = {
+        'courses': queryset
+    }
+
+    return render(request, 'courses/index.html', context)
+
+
+def course_detail(request, course_slug):
+    # Send course, then lesson set
+    course = Course.objects.get(slug=course_slug)
+    lesson_set = course.lesson_set.all()
+
+    context = {
+        'course': course,
+        'lessons': list(lesson_set)
+    }
+
+    return render(request, 'courses/course_detail.html', context)
     
+def lesson_detail(request, course_slug, lesson_slug):
+
+    lesson = Lesson.objects.select_related('course').get(slug=lesson_slug)
+
+    context = {
+        'lesson': lesson
+    }
+
+    return render(request, 'courses/lesson_detail.html', context)
