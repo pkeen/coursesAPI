396272dcff7ea3ca552dc7265f2d90e798b3840f
pete <pkeen7@gmail.com>
2022-10-20 16:47:31 -0600

    remove hardcoded links in templates and replace with url template tag referencing url names

--- courses/urls.py
+++ courses/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:course_slug>/', views.course_detail, name='course_detail'),
-    path('<str:course_slug>/<str:lesson_slug>', views.lesson_detail, name='lesson_detail')
+    path('<str:course_slug>/<str:lesson_slug>/', views.lesson_detail, name='lesson_detail')
 ]
